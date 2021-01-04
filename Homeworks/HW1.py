f13ffd30c20a7f1dd88e80a9ac67ab9133e364c4
Muhammad Farjad Ali Raza <43019983+themohal@users.noreply.github.com>
2021-01-05 01:55:01 +0500

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,7 +1,5 @@
 #Explain your work
-#this for loop which will iterate through 0..a where 'a' can be any number.
-#if a is equal 10 this will print 0 inclusive to 10 exclusive.
-a=10
+
 #Question 1
 for x in range(a):
 	print(a)
